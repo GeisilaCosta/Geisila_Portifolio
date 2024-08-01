87a4265f49ce918c1817746e05c95d6bc8d9695d
GeisilaCosta <geisilascosta@gmail.com>
2024-08-01 19:27:56 -0300

    atualizando o projeto

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/geisila-portifolio/',
+  base: '/Geisila_Portifolio/',
   plugins: [react()],
 })
